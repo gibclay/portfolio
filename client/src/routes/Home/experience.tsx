fc8d2b772f22bf088df05e2941466bd2ef39de02
Monty <momch4il@gmail.com>
2024-05-27 13:43:11 -0400

    added experience section 'cards' sort of

--- client/src/routes/Home/experience.tsx
+++ client/src/routes/Home/experience.tsx
@@ -1,8 +1,26 @@
+import heavy_logo from "@src/assets/heavy/heavy_logo.svg";
+
 export const Experience = () => {
+  let experiences = [
+    {
+      name: "Heavy4Rent",
+      logo: heavy_logo,
+    },
+  ];
   return (
     <>
       <h1 className="h1">Experience</h1>
-      <p>Heavy4Rent?</p>
+      <div className="grid">
+        {/* Map experience from list. */}
+        {experiences.map((experience) => {
+          return (
+            <div className="bg-base-200 p-2 rounded-box">
+              <img src={experience.logo} alt={experience.name + "logo"} />
+              <h3 className="h2 mt-2">{experience.name}</h3>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
